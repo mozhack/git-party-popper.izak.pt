ffad67c86a008d66cabbe601d10cfd4d9259c2a1
Daniel Jesus <30222128+mozhack@users.noreply.github.com>
2023-11-04 22:05:01 +0000

    Initial Release

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+/*
+Author: Mozhack
+Link: https://izak.pt
+Github: https://github.com/mozhack
+Contact: Daniel <mozack@txt.pt>
+*/
+
+function showPartyPopper(){
+	confetti({
+		particleCount: 150, 
+		spread: 60
+	});
+}
